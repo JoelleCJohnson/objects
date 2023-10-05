5a9f1c9ea6b824001692de373b1297a82d413753
Joelle  Johnson <joellej221@gmail.com>
2023-10-05 16:54:23 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const person = {
+firstName: "Todd",
+age: 28,
+}
+// console.log(person.firstName);
+person.age++
+// console.log(person.age)
+person.lastName = "Albert";
+console.log(person.lastName);
